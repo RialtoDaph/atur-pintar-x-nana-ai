30cfe5d0fd2f4b7ebf971ab342c06685e5bf20c6
Alto Daphino <altodaphino@gmail.com>
2026-03-06 21:18:16 +0000

    File changes

--- src/components/transactions/EditTransactionModal.jsx
+++ src/components/transactions/EditTransactionModal.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { X, Settings2 } from "lucide-react";
 import { base44 } from "@/api/base44Client";
+import { useAppSettings } from "@/components/utils/useAppSettings";
 import ManageCategoriesModal from "./ManageCategoriesModal";
 
 const DEFAULT_CATEGORIES = {
